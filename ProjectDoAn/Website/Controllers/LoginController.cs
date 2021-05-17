febfa04fc808633c7cd9a1d51737f85b3070b402
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2021-05-18 00:14:32 +0700

    update

--- ProjectDoAn/Website/Controllers/LoginController.cs
+++ ProjectDoAn/Website/Controllers/LoginController.cs
@@ -13,5 +13,13 @@ namespace Website.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
